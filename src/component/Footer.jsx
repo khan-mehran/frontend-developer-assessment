b6fe9656a2e89f404beb12d08303bffd06b5dfbf
Mehran Khan <mehrankhan@HBKUITs-MacBook-Pro.local>
2024-03-30 00:41:35 +0300

    fix footer css

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 const Footer = () => {
-  return (
-    <footer className="container"> 
-        <p className="text-center text-white py-4">© 2024 Frontend Developer Assessment, All rights reserved.</p>
-    </footer>
-  )
+    return (
+        <footer>
+            <div className="container">
+                <p className="text-center text-white py-4 m-0">© 2024 Frontend Developer Assessment, All rights reserved.</p>
+            </div>
+        </footer>
+    )
 }
 
 export default Footer;
